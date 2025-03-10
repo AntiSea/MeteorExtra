d26efe822df2bfa6acfcf15e1b57ac584d27f51f
unknownsea <seasoank@gmail.com>
2025-03-10 00:41:58 -0700

    Addition + Fix
    Fixed up some stuff, cleaned directory, and added new feature, Auto Respond.

--- src/main/java/net/antisea/meteorextra/MeteorExtra.java
+++ src/main/java/net/antisea/meteorextra/MeteorExtra.java
@@ -1,30 +1,23 @@
 package net.antisea.meteorextra;
 
-import net.antisea.meteorextra.commands.CommandExample;
-import net.antisea.meteorextra.hud.HudExample;
-import net.antisea.meteorextra.modules.ModuleExample;
+import net.antisea.meteorextra.modules.AutoRespond;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
-public class AddonTemplate extends MeteorAddon {
+public class MeteorExtra extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("Meteor Extra");
     public static final HudGroup HUD_GROUP = new HudGroup("Meteor Extra");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
-
-        Modules.get().add(new ModuleExample());
-        Commands.add(new CommandExample());
-        Hud.get().register(HudExample.INFO);
+        LOG.info("Initializing Meteor Extra");
+        Modules.get().add(new AutoRespond());
     }
 
     @Override
---
+++ src/main/java/net/antisea/meteorextra/modules/AutoRespond.java
@@ -0,0 +1,45 @@
+package net.antisea.meteorextra.modules;
+
+import net.antisea.meteorextra.MeteorExtra;
+import meteordevelopment.meteorclient.settings.StringSetting;
+import net.minecraft.client.MinecraftClient;
+import meteordevelopment.meteorclient.events.game.ReceiveMessageEvent;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+
+public class AutoRespond extends Module {
+    private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
+
+    private final Setting<String> respondTo = sgGeneral.add(new StringSetting.Builder()
+        .name("respond-to")
+        .description("The text to respond to.")
+        .defaultValue("hello")
+        .build()
+    );
+
+    private final Setting<String> response = sgGeneral.add(new StringSetting.Builder()
+        .name("response")
+        .description("The text to respond with.")
+        .defaultValue("hi there!")
+        .build()
+    );
+
+    public AutoRespond() {
+        super(MeteorExtra.CATEGORY, "auto-respond", "Automatically respond to specified messages.");
+    }
+
+    @EventHandler
+    private void onChatReceived(ReceiveMessageEvent event) {
+        String message = event.getMessage().getString();
+        if (message.contains(respondTo.get())) {
+            if (response.get().startsWith("/")) {
+                MinecraftClient.getInstance().player.networkHandler.sendCommand(response.get().substring(1));
+            } else {
+                MinecraftClient.getInstance().player.networkHandler.sendChatMessage(response.get());
+            }
+        }
+    }
+
+}
